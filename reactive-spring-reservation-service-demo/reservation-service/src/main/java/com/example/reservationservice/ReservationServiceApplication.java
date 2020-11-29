b39ca8e8a71808a792b07eafb84ef56a01c1e345
ercaany <ercaany@gmail.com>
2020-11-29 19:40:18 +0300

    dockerized mongo and reservation-service

--- reactive-spring-reservation-service-demo/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reactive-spring-reservation-service-demo/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -2,7 +2,6 @@ package com.example.reservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
